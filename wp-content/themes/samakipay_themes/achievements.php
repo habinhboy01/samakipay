f092521b0c449b0ff2dbb7f4273d20bc2752d37e
Phuong Nam <nguyenphuongnam470306@gmail.com>
2022-09-26 15:28:16 +0700

    add

---
+++ wp-content/themes/samakipay_themes/achievements.php
@@ -0,0 +1,42 @@
+<?php
+/*
+
+Template Name: Achievements
+
+*/
+
+get_header(); ?>
+
+	<div class="intro-about">
+		<?php 
+            $image = get_field('img-intro');
+            if( !empty( $image ) ): ?>
+                <img class="img-about" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+        <?php endif; ?>
+
+        <div class="bg-intro2 bg-about">
+
+        	<h1 class="text-about"><?php echo get_field('title-achievements'); ?></h1>
+
+        </div>
+	</div>
+
+	<div class="container bg-certificates">
+		<div class="row">
+			<?php if( have_rows('list-achievements') ): ?>
+	            <?php while( have_rows('list-achievements') ): the_row(); 
+	                $image = get_sub_field('img');
+	                $picture = $image['sizes']['thumbnail']; 
+	                ?>
+
+	                <div class="col-lg-4 col-md-6 col-12">
+	                	<img class="img-certificates img-achievements" src="<?php echo $picture;?>" alt="<?php echo $image['alt'];?>"> 
+	                </div>
+
+	             <?php endwhile; ?>
+        	<?php endif; ?>
+
+		</div>
+	</div>
+
+<?php get_footer(); ?>
